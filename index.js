ef0d54454bf6c32cef1fd4493558511b9b54fd5c
Dennis Kimutai <denniskimkimaiyo@gmail.com>
2024-02-02 00:34:15 +0300

    Added routes

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
 
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.get("/student", function (req, res) {
+  res.render("student-dashboard");
+});
+
+app.use(express.static("views"));
 app.listen(PORT, function () {
-    console.log(`Web server running at: http://localhost:${PORT}`);
-    console.log("Type Ctrl+C to shut down the web server");
+  console.log(`Web server running at: http://localhost:${PORT}`);
+  console.log("Type Ctrl+C to shut down the web server");
 });
